0997d961fc89a9a085b6c7859a31d7da54a73e42
FELIPE BELGINE <felipe.belgine@aircanada.com>
2024-09-03 11:23:11 -0400

    add new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+    puts @article.errors.full_messages # Debugging line
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
 end
