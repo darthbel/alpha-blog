50ad5bf09ebdccc3fa89a97d0ab13b705c4c4ce3
FELIPE BELGINE <felipe.belgine@aircanada.com>
2024-09-06 09:53:04 -0400

    add password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_05_174548) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_05_184253) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,5 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_05_174548) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "email"
   end
 end
